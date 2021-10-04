c3ab9307f1c21282539ed9d39633a1631cf325d4
Riska Amelia <riskaamelia1583@gmail.com>
2021-10-04 14:26:53 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,46 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hallo pak';
+});
+
+Route::get('laravel', function () {
+    return 'Hallo Laravel';
+});
+
+Route::get('beranda', function () {
+    return 'Hallo Selamat datang!';
+});
+
+// Parameter wajib
+//penulisan url contoh about/riska
+Route::get('/about/{nama}', function ($nama) {
+    return 'Nama saya ' . $nama;
+});
+
+Route::get('kontak/{email}', function ($email) {
+    return 'Hubungi : ' . $email;
+});
+
+// opsional parameter
+//beri tanda tanya(?) untuk parameter opsional
+
+Route::get('/nama-saya/{nama?}', function ($nama = null) {
+    return 'Nama saya ' . $nama;
+});
+
+Route::get('/nama/{nama?}', function ($nama = 'wajib diisi') {
+    return 'Nama saya ' . $nama;
+});
+
+// latihan
+Route::get('/luas-segitiga/{tinggi?}/{alas?}', function ($tinggi = 1, $alas = 1) {
+    return 'Hitung Luas Segitiga <br>
+    Tinggi : ' . $tinggi .
+        '<br> Alas : ' . $alas .
+        '<br> L = 1/2 x Alas x Tinggi
+        <br> Hasil : ' . 1 / 2 * ($tinggi * $alas);
 });
